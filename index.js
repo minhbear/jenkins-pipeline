e1118be881e6498e64ed9b8e9fdabbd06cae9dca
Huy Le <huyquoc.le@team.enouvo>
2023-08-20 16:10:09 +0700

    feat: init a simple websever

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const APP_PORT = 3000
+const CONTACT_ROUTE = 'contact'
+
+// A simple server of NodeJs
+
+app.get(`/${CONTACT_ROUTE}`, (req, res) => {
+  const info = {
+    name: 'Nguyen Quang Nhat Minh',
+    email: 'minhbeardev@gmail.com',
+    twitter: 'minhbear_dev'
+  }
+
+  res.json({
+    mes: 'Please contact with the information',
+    info
+  })
+})
+
+app.listen(APP_PORT, () => {
+  console.log(`Server listening on ${APP_PORT}`)
+})
