69d1ee194720b096f5d48e9ad5810a5897d1c71c
Huy Le <huyquoc.le@team.enouvo>
2023-08-22 18:56:20 +0700

    feat: add new mes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get(`/${CONTACT_ROUTE}`, (req, res) => {
   }
 
   res.json({
-    mes: 'Please contact to informationasldfbhlaskdfblsakdfb',
+    mes: 'Please contact to informational',
     info
   })
 })
